aebac01df871854e886860e468191a4bfb884a47
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-11-04 21:31:59 -0500

    Primer Commit

---
+++ bindASocket.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+
